51b588d61f05884d979ef9f8d3ff7695ed47d512
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-15 13:56:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function volume_sphere() {
-    //Write your code here
-  
+let radius = document.getElementById("radius").value
+    if(Number.parseInt(radius)>0){
+        let volume=document.getElementById("volume")
+        let temp=4*Math.PI*Math.pow(radius,3)
+        temp/=3
+        volume.value=temp
+    }
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
