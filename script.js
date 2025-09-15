eaa471f128b73bf2937e5be0f16a8bbf47260e18
harishgattamaneni <103264547+harishgattamaneni@users.noreply.github.com>
2025-09-15 14:14:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function volume_sphere() {
-let radius = document.getElementById("radius").value
-    if(Number.parseInt(radius)>0){
-        let volume=document.getElementById("volume")
-        let temp=4*Math.PI*Math.pow(radius,3)
-        temp/=3
-        volume.value=temp
+function volume_sphere(event) {
+    event.preventDefault(); // Prevent form submission
+    let radius = document.getElementById("radius").value;
+    let radiusValue = parseFloat(radius);
+    let volume = document.getElementById("volume");
+
+    if (!isNaN(radiusValue) && radiusValue >= 0) {
+        let temp = (4 / 3) * Math.PI * Math.pow(radiusValue, 3);
+        volume.innerHTML = `${temp.toFixed(4)}`;
+    } else {
+        volume.textContent = "NaN";
     }
-} 
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
